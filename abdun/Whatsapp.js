cbc6f9d4c9507a4018dc4388c06a07db8388bf53
Javed Iqbal <78470362+ijaved85@users.noreply.github.com>
2023-01-30 09:41:59 +0530

    Added my upi

--- abdun/Whatsapp.js
+++ abdun/Whatsapp.js
@@ -1,13 +1,14 @@
 let dnumber, pnumber, phn, mess, finaltext, enc, phto, link, note;
 let dtype = "D - ";
-let bank = "Dr.Abdun Nur\nPunjab National Bank\nBranch Choki\nA/C No. 1250010101531\nIFSC Code : PUNB0125020";
-note = " \n_***KINDLY SEND THE SCREENSORT OF THE PAYMENT. SO THAT I CAN SEND YOUR MEDICINE NAME.***_";
+let bank = "Dr.Abdun Nur\nPunjab National Bank\nBranch Choki\nA/C No. 1250010101531\nIFSC Code : PUNB0125020\n \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t OR \n Pay using upi - 8346051322";
+note = " \n_***KINDLY SEND THE SCREENSORT OF THE PAYMENT. SO THAT I CAN SEND YOUR MEDICINE NAME.\n***_";
 
 function PatienceM() {
   document.getElementById("dn").innerHTML = "D-";
   dtype = "D - ";
 }
 function PatienceK() {
+  া
   document.getElementById("dn").innerHTML = "K-";
   dtype = "K - ";
 }
@@ -17,11 +18,11 @@ function Message() {
 }
 function Pnb() {
   document.getElementById("Message").style.display = "none";
-  bank = "Dr.Abdun Nur\nPunjab National Bank\nBranch Choki\nA/C No. 1250010101531\nIFSC Code : PUNB0125020\n";
+  bank = "Dr.Abdun Nur\nPunjab National Bank\nBranch Choki\nA/C No. 1250010101531\nIFSC Code : PUNB0125020\n \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t OR \n Pay using upi - 8346051322 \n";
 }
 function Axis() {
   document.getElementById("Message").style.display = "none";
-  bank = "Account Holder Name: DR. ABDUN NUR\nBank : Axis Bank\nAccount number: 912010063125312\nIFSC Code: UTIB0001871\n";
+  bank = "Account Holder Name: DR. ABDUN NUR\nBank : Axis Bank\nAccount number: 912010063125312\nIFSC Code: UTIB0001871\n \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t OR \n Pay using upi - 8346051322 ";
 }
 function Sent() {
   dnumber = document.getElementById("dnum").value;
